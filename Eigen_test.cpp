b74c01f3cabe6b80e7b4c702408f7ad28cc8be91
MorelElian <elian.morel@laposte.net>
2023-04-03 22:31:05 -0400

    yes

---
+++ Eigen_test.cpp
@@ -0,0 +1,10 @@
+#include <Eigen/Core>
+using namespace std;
+using namespace Eigen; 
+template<typename T>
+void test_aot(int size, int repeat)
+{
+    Matrix<T,Dynamic,Dynamic> I = Matrix<T,Dynamic,Dynamic>::Ones(size,size);
+    Matrix<T,Dynamic,Dynamic> m(size,size);
+    for(int i = 0;i <)
+}
---
+++ hello_world_jit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+template <int x>
+[[clang::jit]] void run(){
+    std::cout << "Hello world compiled at run time : x " << x << "\n";
+}
+int main(int argc, char *argv[])
+{
+    int a = std::atoi(argv[1]);
+    run<a>();
+    
+}
